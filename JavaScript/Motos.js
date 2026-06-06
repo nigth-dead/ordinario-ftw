5dcef40f78b6453433228e4e50d7de7e6dcc8c6b
nigth-dead <coppelzenduo@gmail.com>
2026-06-05 22:04:53 -0600

    Motos XML

--- JavaScript/Motos.js
+++ JavaScript/Motos.js
@@ -2,27 +2,27 @@ window.onload = function cargarImagenes(){
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function(){
         const xmlDoc = xhttp.responseXML
-        const cd = xmlDoc.getElementsByTagName("auto")
+        const cd = xmlDoc.getElementsByTagName("moto")
         miFuncion(cd)
     }
-    xhttp.open("GET", "../XML/Autos.xml")
+    xhttp.open("GET", "../XML/Motos.xml")
     xhttp.send()
 }
 
-function miFuncion(cd) {
+function miFuncion(cd){
     let table="";
     let cont = 0;
     for (let i = 0; i < cd.length; i++) {
         if(cont == 0){
             table += "<tr>"
         }
-        table += "<td><p>" + "<img class='img-tarjeta' src='../imgs/coche.png'></p>" +
+        table += "<td><p>" + "<img class='img-tarjeta' src='../imgs/moto.png'></p>" +
         "<p>Marca: " + cd[i].getElementsByTagName("marca")[0].childNodes[0].nodeValue + "</p><p>" +
         "Modelo: " + cd[i].getElementsByTagName("modelo")[0].childNodes[0].nodeValue + "</p><p>" +
         "Año: " + cd[i].getElementsByTagName("anio")[0].childNodes[0].nodeValue + "</p><p>" +
         "Color: " + cd[i].getElementsByTagName("color")[0].childNodes[0].nodeValue + "</p><p>" +
         "Precio: " + cd[i].getElementsByTagName("precio")[0].childNodes[0].nodeValue + "</p><p>" +
-        "Numero de puertas: " + cd[i].getElementsByTagName("noPuertas")[0].childNodes[0].nodeValue + "</p>" +
+        "Cilindrada: " + cd[i].getElementsByTagName("cilindrada")[0].childNodes[0].nodeValue + "</p>" +
         "</td>";
         cont ++;
         if (cont >= 4){
