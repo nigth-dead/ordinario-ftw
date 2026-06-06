e42588288e634fd770e0d2afcb05fa1c507b0d3c
nigth-dead <coppelzenduo@gmail.com>
2026-06-05 21:01:18 -0600

    Login creado

---
+++ JavaScript/Autos.js
@@ -0,0 +1,37 @@
+window.onload = function cargarImagenes(){
+    const xhttp = new XMLHttpRequest();
+    console.log(xhttp)
+    xhttp.onload = function(){
+        const xmlDoc = xhttp.responseXML
+        const cd = xmlDoc.getElementsByTagName("auto")
+        miFuncion(cd)
+    }
+    xhttp.open("GET", "../XML/Autos.xml")
+    xhttp.send()
+}
+
+function miFuncion(cd) {
+    console.log(cd)
+    let table="";
+    let cont = 0;
+    for (let i = 0; i < cd.length; i++) {
+        if(cont == 0){
+            table += "<tr>"
+        }
+        table += "<td><p>" + "<img class='coche' src='../imgs/coche.png'></p>" +
+        "<p>Marca: " + cd[i].getElementsByTagName("marca")[0].childNodes[0].nodeValue + "</p><p>" +
+        "Modelo: " + cd[i].getElementsByTagName("modelo")[0].childNodes[0].nodeValue + "</p><p>" +
+        "Año: " + cd[i].getElementsByTagName("anio")[0].childNodes[0].nodeValue + "</p><p>" +
+        "Color: " + cd[i].getElementsByTagName("color")[0].childNodes[0].nodeValue + "</p><p>" +
+        "Precio: " + cd[i].getElementsByTagName("precio")[0].childNodes[0].nodeValue + "</p>" +
+        "</td>";
+        cont ++;
+        if (cont >= 4){
+            table += "</tr>"
+            cont = 0;
+        }
+    }
+    table += "</tr>"
+    
+    document.getElementById("tabla").innerHTML = table;
+}
---
+++ JavaScript/index.js
@@ -0,0 +1,32 @@
+function Validar(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function(){
+        const xmlDoc = xhttp.responseXML
+        const cd = xmlDoc.getElementsByTagName("usuario")
+        miFuncion(cd)
+    }
+    xhttp.open("GET", "../XML/Usuarios.xml")
+    xhttp.send()
+}
+
+function miFuncion(cd){
+    const usuarioInput = document.getElementById("usuario").value
+    const contrasenaInput = document.getElementById("contrasena").value
+    let loginCorrecto = false
+
+    for(let i = 0; i < cd.length; i++){
+        const usuario = cd[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue
+        const contrasena = cd[i].getElementsByTagName("contrasena")[0].childNodes[0].nodeValue
+
+        if(usuarioInput == usuario && contrasenaInput == contrasena){
+            loginCorrecto = true
+            break
+        }
+    }
+
+    if(loginCorrecto){
+        window.location.href = "HTML/Inicio.html"
+    } else {
+        document.getElementById("error").innerText = "Usuario  o contraseña incorrecta"
+    }
+}
